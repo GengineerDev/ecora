a7a900b39f9d9480fd10337e86c62d8864eec6bc
GengineerDev <pedegloriodavidgenesis@gmail.com>
2023-11-06 11:13:26 +0800

    Add page

--- ecora/src/routes/+page.svelte
+++ ecora/src/routes/+page.svelte
@@ -1,46 +1,27 @@
+<script>
+	import '../styles.css'
+</script>
 
-  <ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Great success!!</ion-card-subtitle>
-		<ion-card-title>Welcome to your app!</ion-card-title>
-	</ion-card-header>
+<style>
+	.logo {
+		width: 100%;
+		max-width: 300px;
+		margin: 0 auto;
+		display: block;
+	
+	}
+</style>
 
-	<ion-card-content>
-		Thank you for using this starter. Click buttons below to open these guides (will
-		open in new window). Don't forget to open DevTools to see this app in mobile mode. Happy coding!!!
-	</ion-card-content>
-
-	<ion-item>
-		<ion-label>Visit Ionic Showcase app with sourceviewer</ion-label>
-		<ion-button href="https://ionicsvelte.firebaseapp.com/" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-
-	<ion-item>
-		<ion-label>Visit Ionic component docs</ion-label>
-		<ion-button
-			href="https://ionicframework.com/docs/components"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte Kit docs</ion-label>
-		<ion-button
-			href="https://kit.svelte.dev/docs/introduction"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte docs</ion-label>
-		<ion-button href="https://svelte.dev/docs" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-</ion-card>
-
-  
+<ion-content>
+	<img src="/plantbox-logo.jpg" alt="PlantBox logo" class="logo"/>
+	<ion-button>Login</ion-button>
+	<ion-button>Login</ion-button>
+	<!-- <ion-list>
+		<ion-item>
+			<ion-input label="Username"></ion-input>
+		</ion-item>
+		<ion-item>
+			<ion-input label="Password" type="password"></ion-input>
+		</ion-item>
+	</ion-list> -->
+</ion-content>
